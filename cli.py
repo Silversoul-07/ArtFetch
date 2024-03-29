013de4106d28b433d3dda7a877449e21d77bb15a
praveen <praveenkumar.e2022@vitstudent.ac.in>
2024-03-30 00:11:15 +0530

    fix

--- cli.py
+++ cli.py
@@ -1,8 +1,7 @@
 from Extensions import scrape_images_from_google
 
 if __name__ == "__main__":
-    query  = "japanese naked women nyotaimori"
-    links = scrape_images_from_google(query)
-
-    with open('test.txt', 'w') as file:
-        file.write('\n'.join(links))
+    query = input("Enter your query: ")
+    limit = int(input("Enter the number of images you want to scrape: "))
+    img_srcs = scrape_images_from_google(query, limit)
+    print(img_srcs)
