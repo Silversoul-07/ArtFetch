cac12ce833d77264f45296aefa57b10cdf128b7f
praveen <praveenkumar.e2022@vitstudent.ac.in>
2024-03-31 18:10:35 +0530

    Updated Bing.py

--- Extensions/Bing.py
+++ Extensions/Bing.py
@@ -1,50 +1,20 @@
-from bs4 import BeautifulSoup
-import requests
-from PIL import Image
-from io import BytesIO
-from fake_useragent import UserAgent
-import os
-import concurrent.futures
-from tqdm import tqdm
-
-def download(link):
-        try:
-            img_obj = requests.get(link.attrs["href"])
-            title = link.attrs["href"].split("/")[-1]
-            try:
-                img = Image.open(BytesIO(img_obj.content))
-                img.save(os.path.join(dir,title), img.format)
-            except:
-                pass
-        except:
-            pass
-
-def BingSearch(topic, dir):
-    headers = {'User-Agent': UserAgent().random}
-    params = {"q": topic}
-
-    r = requests.get("http://www.bing.com/images/search", params=params, headers=headers)
-    if not r.ok:
-        print('Connection Failed!')
-        return
-    else:
-        print('Connection ok')
-
-    soup = BeautifulSoup(r.text, "html.parser")
-    links = soup.find_all("a", {"class": "thumb"})
-
-    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
-        list(tqdm(executor.map(download, links), total=len(links)))
-
-if __name__ == "__main__":
-    BingSearch(
-        topic = "Hot images of Tamil girl",
-        dir = "downloads",
-    )
-
-from bs4 import BeautifulSoup
+import time
+from tqdm import trange
 from selenium import webdriver
-import json
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+options = webdriver.EdgeOptions()
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--ignore-certificate-errors-spki-list')
+options.add_argument('--ignore-ssl-errors')
+driver = webdriver.Edge(options=options)
+wait = WebDriverWait(driver, 10)
+
+query = input("Enter the query: ")
+url = f"http://www.bing.com/images/search?q={query.replace(' ', '+')}"
+url
+driver.get(url)
 
 driver = webdriver.Edge()
 soup = BeautifulSoup(driver.page_source, 'html.parser')
